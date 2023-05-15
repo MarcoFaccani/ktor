a031ad6cd6afa0d71b59430759c7febef905b6c0
MarcoFaccani <m.faccani@alpiantech.com>
2023-05-15 15:36:55 +0200

    first commit

---
+++ src/main/kotlin/com/marcofaccani/Application.kt
@@ -0,0 +1,16 @@
+package com.marcofaccani
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.marcofaccani.plugins.*
+
+fun main() {
+  embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    .start(wait = true)
+}
+
+fun Application.module() {
+  configureSerialization()
+  configureRouting()
+}
